3993d4802a2661ee45624fd44defb68d822623bf
LuckyClover7373 <70354033+LuckyClover7373@users.noreply.github.com>
2023-09-24 22:34:03 +0900

    aa

--- app.js
+++ app.js
@@ -7,16 +7,16 @@ app.get('/', function(req, res){
   let output;
 
   try {
-    const query = req.query.q
+    const query = req.query.q;
 
-    const response = fetch(decodeURI(query))
+    const response = fetch(decodeURI(query));
 
     output = {"result": "success", "response": response};
   }
   catch(e) {
     output = {"result": "error", "error": "Unable to fetch. Name: " + e.name + ", Message: " + e.message};
   }
-  res.send(output)
+  res.send(output);
 });
 
-app.listen(3000, () => console.log("API Server is running..."))
+app.listen(3000, () => console.log("API Server is running..."));
