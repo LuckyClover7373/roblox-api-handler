d0f9a73acaa8f1e0bd13f60eac8c1fcb1eff638e
LuckyClover7373 <70354033+LuckyClover7373@users.noreply.github.com>
2023-09-24 21:05:18 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,46 +1,21 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const nunjucks = require('nunjucks');
+var output; // Declare a variable to store the response data.
 
-var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'njk');
-nunjucks.configure('views', { 
-  express: app,
-  watch: true,
-});
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+  try {
+    // Retrieve the "q" query parameter from the request URL.
+    var query = e.parameter["q"];
+    
+    // Make an HTTP GET request to the URL specified in the "q" parameter.
+    var response = UrlFetchApp.fetch(decodeURIComponent(query));
+    
+    // Create a success response with "result" set to "success" and "response" containing the fetched JSON data.
+    output = {"result": "success", "response": JSON.parse(response.getContentText())};
+  }
+  catch(e) {
+    // Handle any exceptions (errors) that occurred during the try block.
+    
+    // Create an error response with "result" set to "error" and an "error" message.
+    output = {"result": "error", "error": "Unable to fetch. Name: " + e.name + ", Message: " + e.message};
+  }
+  
+  // Create a JSON response from the output object and set the MIME type to JSON.
+  return ContentService.createTextOutput(JSON.stringify(output)).setMimeType(ContentService.MimeType.JSON);
