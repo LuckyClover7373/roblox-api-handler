db6ab587c5cca9b457d80258db4a1db6b3caf1c1
LuckyClover7373 <70354033+LuckyClover7373@users.noreply.github.com>
2023-09-24 21:17:56 +0900

    e

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
-var output; // Declare a variable to store the response data.
+const express = require('express');
+const url = require('url');
+const querystring = require('querystring');
 
-  try {
-    // Retrieve the "q" query parameter from the request URL.
-    var query = e.parameter["q"];
-    
-    // Make an HTTP GET request to the URL specified in the "q" parameter.
-    var response = UrlFetchApp.fetch(decodeURIComponent(query));
-    
-    // Create a success response with "result" set to "success" and "response" containing the fetched JSON data.
-    output = {"result": "success", "response": JSON.parse(response.getContentText())};
-  }
-  catch(e) {
-    // Handle any exceptions (errors) that occurred during the try block.
-    
-    // Create an error response with "result" set to "error" and an "error" message.
-    output = {"result": "error", "error": "Unable to fetch. Name: " + e.name + ", Message: " + e.message};
-  }
-  
-  // Create a JSON response from the output object and set the MIME type to JSON.
-  return ContentService.createTextOutput(JSON.stringify(output)).setMimeType(ContentService.MimeType.JSON);
+const app = express();
+
+app.get('/', function(req, res){
+  res.send('id: ' + req.query.id);
+});urn ContentService.createTextOutput(JSON.stringify(output)).setMimeType(ContentService.MimeType.JSON);
+});
+
+app.listen(3000, () => console.log("API Server is running..."))
