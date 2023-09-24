525620dcdf230ebd658d15028a5f07310f0dab7d
LuckyClover7373 <70354033+LuckyClover7373@users.noreply.github.com>
2023-09-24 21:19:47 +0900

    aa

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const app = express();
 
 app.get('/', function(req, res){
   res.send('id: ' + req.query.id);
-});urn ContentService.createTextOutput(JSON.stringify(output)).setMimeType(ContentService.MimeType.JSON);
 });
 
 app.listen(3000, () => console.log("API Server is running..."))
